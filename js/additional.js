95a6a07cd0b7104b65a3b405aa65f17616eb30f5
Maz <mazharul2007@gmail.com>
2012-12-28 12:13:47 +0800

    new changes

--- js/additional.js
+++ js/additional.js
@@ -2,20 +2,23 @@ $(document).ready(function(){
 	$("#about").css("display", "none");
 	$("#contact").css("display", "none");
 
-	$("#abt").click(function(){
+	$("#abt").click(function(e){
+
+		e.preventDefault();
 		$("#contact").css("display", "none");
 		$("#mainContent").css("display", "none");
 		$("#about").css("display", "block");
 	});
 
-	$("#cont").click(function(){
-		
+	$("#cont").click(function(e){
+		e.preventDefault();
 		$("#mainContent").css("display", "none");
 		$("#about").css("display", "none");
 		$("#contact").css("display", "block");
 	});
 
-	$("#mnCon").click(function(){
+	$("#mnCon").click(function(e){
+		e.preventDefault();
 		$("#contact").css("display", "none");
 		$("#mainContent").css("display", "block");
 		$("#about").css("display", "none");
